45f8607adfeda19083cc174dbb3e352ca1b46383
saikumar <sai680513@gmail.com>
2024-06-13 17:14:52 +0530

    install setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='AP EAPCET 2024 College predictor',
+    version='0.0.1',
+    author='Sai kumar Seela',
+    author_email='sai680513@gmail.com',
+    install_requires=["pandas","PyPDF2"],
+    packages=find_packages()
+)
